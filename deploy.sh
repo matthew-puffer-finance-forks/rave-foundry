d325402eaa8205c4a572e076858ffc58fc1066a2
Matthew Roberts <matthew@puffer.fi>
2023-07-31 23:49:36 +0000

    Saving progress.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,32 @@
 
+# Utils.
+deploy_addr="python3 -c \"import re, sys; x = sys.stdin.read(); "
+deploy_addr+="print(re.findall('Deployed to: 0x([^\s]+)', x)[0],)\""
 
-rm -rf src/*
-cp -R ../rave/src/* src
-forge create --rpc-url "http://127.0.0.1:8545" --private-key "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6" src/RAVE.sol:RAVE
+chmod -R 777 lib/rave
+rm -rf lib/rave
+cp -R ../rave lib/rave
+chmod -R 777 lib/rave
+deploy_out=$(forge create --rpc-url "http://127.0.0.1:8545" --private-key "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6" src/Main.sol:Main)
+rave_addr=$(eval "echo '$deploy_out' | $deploy_addr")
+
+
+
+
+
 pushd ../rave/test/scripts > /dev/null
 rave_inputs=$(./raveEnclaveVerify.sh)
 popd > /dev/null
 
-calldata=$(cast calldata "rave(bytes,bytes,bytes,bytes,bytes,bytes32,bytes32)" $rave_inputs
-cast send $RAVE_ADDRESS $CALLDATA
+calldata=$(cast calldata "rave(bytes,bytes,bytes,bytes,bytes,bytes32,bytes32)" "$rave_inputs")
+
+echo $calldata
+
+exit
+out=$(cast send $rave_addr $calldata)
+echo $out
+
+
+#Deployer: 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720
+#Deployed to: 0xA15BB66138824a1c7167f5E85b957d04Dd34E468
+#Transaction hash: #0x3278a10b82f6c3d9a4460ac4bebfb1fc165a8e05853f73d996169674d8e9b1a2
--- src/Main.sol
+++ src/Main.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: MIT
-// Original source: https://github.com/JonahGroendal/asn1-decode
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import { RAVE } from "rave/src/RAVE.sol";
 
-contract Main {
-    // Unpack first byte index
+import { RAVE } from "../lib/rave/src/RAVE.sol";
+
+contract Main is RAVE {
+ 
 }
